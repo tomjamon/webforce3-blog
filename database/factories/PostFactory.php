8b619d9d10eafa6fdaa2e4db16869be4fcc7c1aa
Tom Jamon <contact@tomjamon.com>
2019-08-21 09:59:53 +0200

    Hotfix PostFactory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,8 +1,8 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\Post;
-use Illuminate\Support\Str;
+use App\Models\Post;
+use Illuminate\ Support\Str;
 use Faker\Generator as Faker;
 
 /*
